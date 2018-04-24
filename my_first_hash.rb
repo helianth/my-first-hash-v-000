6897fcb2866aa36141df797a9a674e597442f040
Lamia Haider <lamia.haider@gmail.com>
2018-04-24 04:10:28 +0000

    automatically backed up by learn

--- my_first_hash.rb
+++ my_first_hash.rb
@@ -1,6 +1,5 @@
 def my_hash
-  # use the literal constructor to set the variable, my_hash, equal to a hash with key/value pairs of your choice.
-
+  my_hash = {"sailor moon" => "usagi", "sailor mars" => "rei", "sailor mercury" => "ami"}
   
 end
 
